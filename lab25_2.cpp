f1a4bcfdb129ddf3a167a53e753d14a4fe545d18
SukriTM <micsu.ky@gmail.com>
2023-03-16 00:38:54 +0700

    Lab 25 completed

--- lab25_2.cpp
+++ lab25_2.cpp
@@ -24,7 +24,27 @@ int main(){
 	line_up.insert(loc,"Oscar");
 	
 	//Write your code here
-	
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+	loc = find(line_up.begin(),line_up.end(),"Alice");
+	line_up.erase(loc);
+	loc = find(line_up.begin(),line_up.end(),"Oscar");
+	line_up.erase(loc);
+	loc = find(line_up.begin(),line_up.end(),"Luffy");
+	line_up.insert(loc,"Narutu");
+	line_up.push_front("Prayath");
+	loc = find(line_up.begin(),line_up.end(),"Prayath");
+	line_up.insert(loc,"Tony");
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.erase(loc);
+	loc = find(line_up.begin(),line_up.end(),"Prayath");
+	line_up.erase(loc);
+	loc = find(line_up.begin(),line_up.end(),"Tony");
+	line_up.erase(loc);
+	loc = find(line_up.begin(),line_up.end(),"Luffy");
+	line_up.erase(loc);
+
 	printList(line_up);
 		
 	return 0;
